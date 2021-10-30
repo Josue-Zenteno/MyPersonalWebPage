509c754a3267ee97c105e258e64cd8b6282afb51
Josue-Zenteno <josuotp@gmail.com>
2021-10-30 18:03:21 +0200

    resumeData.js
    Created resumeData to store all the information

---
+++ src/utils/resumeData.js
@@ -0,0 +1,30 @@
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const resumeData = {
+  name: "Josue Carlos Zenteno Yave",
+  title: "IT Student",
+  birthday: "08/08/2000",
+  email: "josuoptp@gmail.com",
+  phone: "609118725",
+  socials: {
+    Twitter: {
+      link: "https://twitter.com/SpicyJoss",
+      text: "My Twitter",
+      icon: <TwitterIcon />,
+    },
+    Linkedin: {
+      link: "https://www.linkedin.com/in/josue-zenteno/",
+      text: "My LinkedIn",
+      icon: <LinkedInIcon />,
+    },
+    Github: {
+      link: "https://github.com/Josue-Zenteno",
+      text: "My GitHub",
+      icon: <GitHubIcon />,
+    },
+  },
+};
+
+export default resumeData;
