c8b5d8101e261c2e7dcd46472f0187557310a839
Josue-Zenteno <josuotp@gmail.com>
2021-10-30 23:45:27 +0200

    Header.jsx v1.0.0

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,56 @@
-export default function Header(){
+import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink, withRouter } from 'react-router-dom';
+import { HomeRounded, Telegram } from "@mui/icons-material"
+import resumeData from '../../utils/resumeData';
+import CustomButton from '../Button/CustomButton';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'
+
+function Header(props){
+    const pathName = props?.location?.pathName;
+
     return(
-        <div>
-            This is a header component
-        </div>
+        <Navbar expand="lg" sticky='top' className='header'>
+            {/* Home Link */}
+            <Nav.Link as={NavLink} to={"/"} className={'header_navlink'}>
+                <Navbar.Brand className='header_home'>
+                    <HomeRounded/>
+                </Navbar.Brand>
+            </Nav.Link>
+
+            <Navbar.Toggle/>
+
+            <Navbar.Collapse>
+                <Nav className='header_left'>
+                    {/* Resume Link */}
+                    <Nav.Link 
+                    as={NavLink} 
+                    to='/' 
+                    className={pathName === '/' ? 'header_link_active' : 'header_link'}>
+                        Resume
+                    </Nav.Link>
+
+                    {/* Portfolio Link */}
+                    <Nav.Link 
+                    as={NavLink} 
+                    to='/portfolio' 
+                    className={pathName === '/portfolio' ? 'header_link_active' : 'header_link'}>
+                        Portfolio
+                    </Nav.Link>
+                </Nav>
+
+                <div className='header_right'>
+                    {Object.keys(resumeData.socials).map(key => (
+                        <a href={resumeData.socials[key].link} target="_blank" rel="noreferrer">
+                            {resumeData.socials[key].icon}
+                        </a>
+                    ))}
+                    <CustomButton text={"Hire Me"} icon={<Telegram/>}/>
+                </div>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
+
+export default withRouter(Header);
