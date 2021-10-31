101865e068d5597d45df00559038bdcf9f1ba875
Josue-Zenteno <josuotp@gmail.com>
2021-10-31 22:46:28 +0100

    Portfolio.jsx v1.0.0
    Created the basic structure

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,94 @@
+import { useState } from 'react';
+import { Grid, Tabs, Tab, Typography, CardActionArea, Card, CardMedia, CardContent, Grow, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material'
+import resumeData from '../../utils/resumeData'
+
+import './Portfolio.css'
+
 export default function Portfolio(){
+
+    const [tabValue, setTabValue] = useState('All');
+    const [projectDialog, setProjectDialog] = useState(false);
+
     return(
-        <div>
-            This is a portfolio component
-        </div>
+        <Grid container className='section pb_45 pt_45'>
+            {/* Title */}
+            <Grid item>
+                <Grid item className='section_title mb_30'>
+                    <span></span>
+                    <Typography variant='h6' className='section_title_text'>
+                        Portfolio
+                    </Typography>
+                </Grid>
+            </Grid>
+
+            {/* TabBar */}
+            <Grid item xs={12}>
+                {/* Tabs */}
+                <Tabs 
+                value={tabValue} 
+                indicatorColor='white' 
+                className='custom_tabs' 
+                onChange={(event, newValue) => setTabValue(newValue)}>
+                    <Tab label='All' value='All' className={tabValue === 'All' ? 'customTabs_item active' : 'cutomTabs_item'} />
+
+                    {[...new Set(resumeData.projects.map(project => project.tag))].map(tag => (
+                        <Tab label={tag} value={tag} className={tabValue === 'All' ? 'customTabs_item active' : 'cutomTabs_item'}/>
+                    ))}
+                </Tabs>
+            
+                {/* Projects */}
+                <Grid item xs={12}>
+                    <Grid container spacing={2}>
+                        {resumeData.projects.map(project => (
+                            <>
+                                {tabValue === project.tag || tabValue === 'All' ? (
+                                    <Grid item>
+                                        <Grow in timeout={1000}>
+                                            <Card className='customCard' onClick={() => setProjectDialog(project)}>
+                                                <CardActionArea>
+                                                    <CardMedia 
+                                                    className='customCard_image' 
+                                                    image={project.image} 
+                                                    title={project.title}/>
+                                                    <CardContent>
+                                                        <Typography className='customCard_title'>
+                                                            {project.title}
+                                                        </Typography>
+                                                        <Typography variant='body2' className='customCard_description'>
+                                                            {project.caption}
+                                                        </Typography>
+                                                    </CardContent>
+                                                </CardActionArea>
+                                            </Card>
+                                        </Grow>
+                                    </Grid>
+                                ) : null}
+                            </>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
+            
+            {/* Project Dialog */}
+            <Dialog open={projectDialog} onClose={() => setProjectDialog(false)}>
+                <DialogTitle onClose={() => setProjectDialog(false)}>
+                    {projectDialog.title}
+                </DialogTitle>
+                <img src='' alt='' className='projectDialog_image'/>
+                <DialogContent>
+                    <Typography className='projectDialog_description'>
+                        {projectDialog.description}
+                    </Typography>
+                </DialogContent>
+                <DialogActions className='projectDialog_actions'>
+                    {projectDialog?.links?.map(link => (
+                        <a href={link.link} target='_blank' rel='noreferrer' className='projectDialog_icon'>
+                            {link.icon}
+                        </a>
+                    ))}
+                </DialogActions>
+            </Dialog>
+
+        </Grid>
     );
 };
