8e24472599b0a1c512368f5fbd719376f7394a7f
Josue-Zenteno <josuotp@gmail.com>
2021-10-31 20:58:15 +0100

    Resume.jsx v3.0.0
    Added Services section
    Added Skills section

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,17 +1,17 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid, Icon, Paper, Typography } from '@mui/material'
 import CustomTimeLine, {CustomTimeLineSeparator} from '../../components/TimeLine/TimeLine'
 import WorkIcon from '@mui/icons-material/Work';
 import SchoolIcon from '@mui/icons-material/School';
 
 import resumeData from '../../utils/resumeData';
 import './Resume.css'
-import { TimelineItem, TimelineContent } from '@mui/lab';
+import { TimelineItem, TimelineContent, TimelineDot } from '@mui/lab';
 
 export default function Resume(){
     return(
         <>
             {/* About me*/}
-            <Grid container className='section pb_45'>
+            <Grid container className='section pb_45 pt_45'>
                 <Grid item className='section_title mb_30'>
                     <span></span>
                     <Typography variant='h6' className='section_title_text'>
@@ -26,7 +26,7 @@ export default function Resume(){
             </Grid>
 
             {/* Education and Experiences*/}
-            <Grid container className='section'>
+            <Grid container className='section pb_45'>
                 <Grid item className='section_title mb_30'>
                     <span></span>
                     <Typography variant='h6' className='section_title_text'>
@@ -84,13 +84,63 @@ export default function Resume(){
             </Grid>
 
             {/* Services */}
-            <Grid container className='section'>
-                
-            </Grid>
+            <Grid container className='section pb_45'>
+                <Grid item className='section_title mb_30'>
+                    <span></span>
+                    <Typography variant='h6' className='section_title_text'>
+                        My Services
+                    </Typography>
+                </Grid>
 
-            {/* Skills and Contact  */}
-            <Grid container className='section'>
-                
+                <Grid item xs={12}>
+                    <Grid container spacing={3} justify='space-around'>
+                        {resumeData.services.map(service =>(
+                            <Grid item xs={12} sm={6} md={3}>
+                                <div className='service'>
+                                    <Icon className='service_icon'>
+                                        {service.icon}
+                                    </Icon>
+                                    <Typography variant='h6' className='service_title'>
+                                        {service.title}
+                                    </Typography>
+                                    <Typography variant='body2' className='service_description'>
+                                        {service.description}
+                                    </Typography>
+                                </div>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
+            
+            {/* Skills */}
+            <Grid container className='section pb_45'>
+                <Grid item className='section_title mb_30'>
+                    <span></span>
+                    <Typography variant='h6' className='section_title_text'>
+                        Skills
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Grid container spacing={3} justify='space-between' className='p_15'>
+                    {resumeData.skills.map(skill=>(
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Paper elevation={0} className='skill'>
+                                <Typography variant='h6' className='skill_title'>
+                                    {skill.title}
+                                </Typography>
+                                {skill.description.map(element =>(
+                                    <Typography variant='body2' className='skill_description'>
+                                        <TimelineDot variant={'outlined'} className='timeline_dot'/>
+                                        {element}
+                                    </Typography>
+                                ))}
+                            </Paper>
+                        </Grid>
+                    ))}
+                    </Grid>
+                </Grid>
             </Grid>
         </>
     );
