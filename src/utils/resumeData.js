1fe57e58b9f7ffb049fd621c666251998e87edf6
Josue-Zenteno <josuotp@gmail.com>
2021-10-31 18:57:04 +0100

    resumeData.js v0.0.4
    Added more info

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -27,6 +27,58 @@ const resumeData = {
   },
   about:
     "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+  experiencies: [
+    {
+      title: "Work 1",
+      date: "2018 - 2019",
+      description:
+        "My first job Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+    {
+      title: "Work 2",
+      date: "2019 - 2020",
+      description:
+        "My first job Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+    {
+      title: "Work 3",
+      date: "2020 - 2021",
+      description:
+        "My first job Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+    {
+      title: "Work 4",
+      date: "2021 - Present",
+      description:
+        "My first job Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+  ],
+  educations: [
+    {
+      title: "Education 1",
+      date: "2018 - 2019",
+      description:
+        "My first education Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+    {
+      title: "Education 2",
+      date: "2019 - 2020",
+      description:
+        "My first education Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+    {
+      title: "Education 3",
+      date: "2020 - 2021",
+      description:
+        "My first education Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+    {
+      title: "Education 4",
+      date: "2021 - Present",
+      description:
+        "My first education Lorem Ipsum is not simply random text. It has roots in a piece",
+    },
+  ],
 };
 
 export default resumeData;
