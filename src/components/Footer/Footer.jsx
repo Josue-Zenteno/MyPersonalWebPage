f3f039e45659fef5b101683aa88420170a7c4245
Josue-Zenteno <josuotp@gmail.com>
2021-10-31 16:24:51 +0100

    Footer.jsx v1.0.0
    Footer completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
+import { Typography } from '@mui/material';
+import resumeData from '../../utils/resumeData'
+
+import './Footer.css'
+
 export default function Footer(){
     return(
-        <div>
-            This is a footer component
+        <div className= "footer">
+            <div className='footer_left'>
+                <Typography className='footer_name'>
+                    {resumeData.name}
+                </Typography>
+            </div>
+            <div className='footer_right'>
+                <Typography className='footer_copyright'>
+                    {"Designed and developed by "}
+                    <a href='/' target='_blank' rel='noreferrer'> 
+                        {resumeData.name}
+                    </a>
+                </Typography>
+            </div>
         </div>
     );
 };
