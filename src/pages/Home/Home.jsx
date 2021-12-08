74ccbd6dd6620bf058f3a28ce00187796cf7b205
Josue-Zenteno <josuotp@gmail.com>
2021-12-08 16:07:17 +0100

    Home.jsx v1.0.0
    Created a new page to place the About me section

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import { Grid, Typography } from '@mui/material'
+import resumeData from '../../utils/resumeData';
+//import SpicyBanner from '../../assets/images/Spicy_Banner.png'
+import SpicyBannerLight from '../../assets/images/Spicy_Banner_Light.png'
+import './Home.css'
+
+export default function Home(){
+    return (
+        <Grid container  className='section pb_45 pt_20'>
+            <Grid item className='section_title mb_30'>
+                <span></span>
+                <Typography variant='h6' className='section_title_text'>
+                    ¿Quien soy?
+                </Typography>
+            </Grid>
+
+            <Grid item>
+                <img src={SpicyBannerLight} alt='SpicBanner' className='Banner pb_45'>
+                </img>
+            </Grid>
+
+            <Grid item >
+                <Typography variant='body2' className='aboutme_text'>
+                    {resumeData.about}
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+}
