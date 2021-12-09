36ebe9cc64417110240368222bebd5f628fffdcb
Josue-Zenteno <josuotp@gmail.com>
2021-12-09 17:01:28 +0100

    CustomButton.jsx v2.0.1
    Changed the behavior of the pdf download

--- src/components/Button/CustomButton.jsx
+++ src/components/Button/CustomButton.jsx
@@ -5,7 +5,7 @@ import './CustomButton.css'
 export default function CustomButton({text, icon}){
     return(
         // Button
-        <Button className="custom_btn" component="a" href={Resume_es} download={Resume_es} title="Josue-Zenteno">
+        <Button className="custom_btn" component="a" href={Resume_es} target="_blank" title="Josue-Zenteno">
             <span className="btn_text">{text} {icon}</span>
         </Button>
     );
